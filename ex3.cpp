d97f2817c05f1e95b22b07732472a76cfa805ecb
Malingen851 <103818801+Malingen851@users.noreply.github.com>
2022-04-16 17:35:20 +0800

    Add files via upload

---
+++ ex3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char p_alpha, p_alpha_1;
+    int i = 0, j = 0;
+    cout << "Character (lowercase)" << "\t" << "Integer Value" << "\t" << "Character (uppercase)" << "\t" << "Integer Value"<< endl;
+    for (p_alpha = 'a', p_alpha_1 = 'A'; p_alpha <= 'z' && p_alpha_1 <= 'Z'; ++p_alpha, ++p_alpha_1){
+        cout << p_alpha << "\t" << "\t" << "\t" << p_alpha + i << "\t" << "\t" << p_alpha_1 << "\t" << "\t" << "\t" << p_alpha_1 + j << endl;
+    }
+    system ("pause");
+    system ("cls");
+    return 0;
+}
